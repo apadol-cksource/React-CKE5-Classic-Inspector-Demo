9b056fd22e08371b92eb04aa50e517fd3fc9202a
apadol-cksource <106534095+apadol-cksource@users.noreply.github.com>
2022-09-21 22:01:25 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     <div className="App">
       <CKEditor
         editor={ClassicEditor}
-        data="<p>Hello from CKEditor 5!</p><h1>See inpector in action</h1><ul><li>Check the Model</li><li>See the View (output)</li><li>Check available commands</li></ul>"
+        data="<p>Hello from CKEditor 5!</p><h1>Try the inspector below</h1><ul><li>Check the Model</li><li>See the View</li><li>Check available commands</li></ul>"
         onReady={(editor) => {
           console.log('CKEditor React Component is ready to use!', editor);
           CKEditorInspector.attach(editor);
